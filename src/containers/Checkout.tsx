0b826359a1753f14b0c44f6a61a6ce5a3a649f7d
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2021-02-08 19:34:27 -0500

    checkout information page

--- src/containers/Checkout.tsx
+++ src/containers/Checkout.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import '../styles/components/Checkout.css';
 
 const Checkout = () => {
     return (
-        <div>
-            Checkout
+        <div className="checkout">
+            <div className="checkout-content">
+                <h3>Lista de pedidos</h3>
+                <div className="checkout-item">
+                    <div className="checkout-element">
+                        <h4>Item name</h4>
+                        <span>10$</span>
+                    </div>
+                    <button type="button">Eliminar</button>
+                </div>
+            </div>
+            <div className="checkout-sidebar">
+                <h3>Precio total: $10</h3>
+                <button type="button">Continuar pedido</button>
+            </div>
         </div>
     )
 }
--- src/containers/Information.tsx
+++ src/containers/Information.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import '../styles/components/Information.css';
 
 const Information = () => {
     return (
-        <div>
-            Information
+        <div className="information">
+            <div className="informatio-content">
+                <div className="information-head">
+                    Información de contacto
+                    </div>
+                <div className="information-form">
+                    <form>
+                        <input type="text" placeholder="Nombre completo..." name="name" />
+                        <input type="text" placeholder="Correo electrónico" name="email" />
+                        <input type="text" placeholder="Dirección" name="address" />
+                        <input type="text" placeholder="Departamento" name="dpto" />
+                        <input type="text" placeholder="Ciudad" name="city" />
+                        <input type="text" placeholder="País" name="country" />
+                        <input type="text" placeholder="Estado" name="state" />
+                        <input type="text" placeholder="Código postal" name="cp" />
+                        <input type="text" placeholder="Teléfono" name="phone" />
+                    </form>
+                </div>
+                <div className="information-buttons">
+                    <div className="information-back">
+                        Regresar
+                    </div>
+                    <div className="information-next">
+                        Pagar
+                    </div>
+                </div>
+            </div>
+            <div className="information-sidebar">
+                <h3>Pedido:</h3>
+                <div className="information-item">
+                    <div className="information-element">
+                        <h4>Item name</h4>
+                        <span>10$</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
